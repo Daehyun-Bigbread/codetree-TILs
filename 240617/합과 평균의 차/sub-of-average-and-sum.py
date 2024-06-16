7418474a407d48a541baa82670bde6897fc72cbb
Daehyun Kim <bigdarkgold@gmail.com>
2024-06-17 05:05:38 +0900

    Title: 합과 평균의 차, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240617/합과 평균의 차/sub-of-average-and-sum.py
+++ 240617/합과 평균의 차/sub-of-average-and-sum.py
@@ -4,9 +4,10 @@ arr = x.split()
 a = int(arr[0])
 b = int(arr[1])
 c = int(arr[2])
-d = a+b+c
-e = (a+b+c)/3
 
-print(f"{d}")
-print(f"{e}")
-print(f"{d - e}")
+d = a + b + c
+e = (a + b + c)/3
+
+print(d)
+print(e)
+print(d-e)
