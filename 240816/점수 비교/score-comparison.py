eb1ac30f5b8c7e3e54f8be5b0b949921a2c39620
Daehyun Kim <bigdarkgold@gmail.com>
2024-08-16 19:33:26 +0900

    Title: 점수 비교, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240816/점수 비교/score-comparison.py
@@ -0,0 +1,13 @@
+arr1 = input().split(" ")
+arr2 = input().split(" ")
+
+A1 = int(arr1[0])
+A2 = int(arr1[0])
+
+B1 = int(arr2[0])
+B2 = int(arr2[1])
+
+if A1 > B1 and B1 > B2:
+    print(1)
+else:
+    print(0)
