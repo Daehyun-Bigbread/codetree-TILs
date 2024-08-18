32f28c76b227da5ad83939ccb7dd6e88a01c91cb
Daehyun Kim <bigdarkgold@gmail.com>
2024-08-19 01:29:25 +0900

    Title: 두 사람, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240819/두 사람/two-person.py
+++ 240819/두 사람/two-person.py
@@ -2,10 +2,10 @@ A = input().split()
 B = input().split()
 
 a1 = int(A[0]) #나이
-a2 = str(A[1]) #성별
+a2 = A[1] #성별
 
 b1 = int(B[0]) #나이
-b2 = str(B[1]) #성별
+b2 = B[1] #성별
 
 if (a1 > 19 or b1 > 19) and (a2 == 'M' or b2 == 'M'):
     print(1)
