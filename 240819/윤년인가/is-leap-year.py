664c229da70fa1bf9e2f07afea74c0667227109f
Daehyun Kim <bigdarkgold@gmail.com>
2024-08-19 21:47:32 +0900

    Title: 윤년인가, Time: 99ms, Memory: 24MB, Status: Passed - CodeTree

--- 240819/윤년인가/is-leap-year.py
+++ 240819/윤년인가/is-leap-year.py
@@ -1,9 +1,10 @@
 y = int(input())
 
-if y % 4 == 0:
+if y % 400 == 0:
    print("true")
 elif y % 100 == 0:
-    if y % 400 != 0:
-        print("false")
+    print("false")
+elif y % 4 == 0:
+    print("true")
 else:
     print("false")
