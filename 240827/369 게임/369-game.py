5cbe00c928282404ab2227a9ce66e952a79c1951
Daehyun Kim <bigdarkgold@gmail.com>
2024-08-27 01:30:37 +0900

    Title: 369 게임, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240827/369 게임/369-game.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for i in range(1, n+1):
+    if n % 3 == 0:
+        print("0", end=" ")
+    elif i % 10 == 3 or i % 10 == 6 or i % 10 == 9: # 10으로 나누었을때 나머지가 3,6,9 (한자리 수)
+        print("0", end=" ")
+    elif i // 10 == 3 or i // 10 == 6 or i // 10 == 9: # 10으로 나누었을때 몫이 3,6,9
+        print("0", end=" ")
+    else:
+        print(i, end=" ")
