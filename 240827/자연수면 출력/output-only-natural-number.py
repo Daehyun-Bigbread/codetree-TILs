e01b3f203d8a3be5381be8337b151e12221c64a8
Daehyun Kim <bigdarkgold@gmail.com>
2024-08-27 01:07:26 +0900

    Title: 자연수면 출력, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240827/자연수면 출력/output-only-natural-number.py
+++ 240827/자연수면 출력/output-only-natural-number.py
@@ -2,8 +2,8 @@ arr = input().split()
 a, b = int(arr[0]), int(arr[1])
 
 if isinstance(a, int):
-    for i in range(1, b+1):
-        print(a, end="")
-else:
     if a <= 0:
         print(0)
+    else:
+        for i in range(1, b+1):
+            print(a, end="")
