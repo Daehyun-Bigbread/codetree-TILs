7b436d0d4a9905a2726fee871423f40f8ef0c969
Daehyun Kim <bigdarkgold@gmail.com>
2024-08-27 00:55:21 +0900

    Title: 높은 수에서 낮은 수까지, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240827/높은 수에서 낮은 수까지/from-high-to-low.py
+++ 240827/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -1,5 +1,9 @@
 arr = input().split()
 a, b = int(arr[0]), int(arr[1])
 
-for i in range(b, a-1, -1):
-    print(i, end=" ")
+if b >= a:
+    for i in range(b, a-1, -1):
+        print(i, end=" ")
+else:
+    for i in range(a, b-1, -1)
+        print(i, end=" ")
