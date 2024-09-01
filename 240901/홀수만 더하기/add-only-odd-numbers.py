1de445ed6a5678fe8a39ed055665a219ad2cc38c
Daehyun Kim <bigdarkgold@gmail.com>
2024-09-01 17:37:59 +0900

    Title: 홀수만 더하기, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240901/홀수만 더하기/add-only-odd-numbers.py
+++ 240901/홀수만 더하기/add-only-odd-numbers.py
@@ -3,6 +3,6 @@ sum_val = 0
 
 for _ in range(n):
     a = int(input())
-    if (a % 2 == 1 and a % 3 == 0):
-        sum_val =+ a
+    if a % 2 == 1 and a % 3 == 0:
+        sum_val += a
 print(sum_val)
