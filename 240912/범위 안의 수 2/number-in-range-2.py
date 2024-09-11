44cf19ae461580a62a7f470c5d093e5ed4ddc184
Daehyun Kim <bigdarkgold@gmail.com>
2024-09-12 01:57:28 +0900

    Title: 범위 안의 수 2, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240912/범위 안의 수 2/number-in-range-2.py
+++ 240912/범위 안의 수 2/number-in-range-2.py
@@ -1,7 +1,12 @@
 sum_val = 0
+cnt = 0
 
 for i in range(10):
     a = int(input())
-    sum_val += a
+    if a >= 0 and a <= 200:
+        sum_val += a
+        cnt += 1
 
-print(sum(sum_val))
+avg = sum_val / cnt
+
+print(f"{sum_val} {avg:.1f}")
