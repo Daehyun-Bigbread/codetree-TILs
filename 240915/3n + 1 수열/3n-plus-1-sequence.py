c7658ed0597813f0955eb5e8839dd61d377a5534
Daehyun Kim <bigdarkgold@gmail.com>
2024-09-15 16:24:25 +0900

    Title: 3n + 1 수열, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240915/3n + 1 수열/3n-plus-1-sequence.py
@@ -0,0 +1,16 @@
+n = int(input())
+count = 0
+
+while True:
+    if n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = (3 * n) + 1
+    else:
+        break
+    
+    count += 1
+
+
+print(count)
