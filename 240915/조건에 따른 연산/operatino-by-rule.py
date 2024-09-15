980bf2b34f424522ccced6f43768c28ac512efcd
Daehyun Kim <bigdarkgold@gmail.com>
2024-09-15 17:10:44 +0900

    Title: 조건에 따른 연산, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240915/조건에 따른 연산/operatino-by-rule.py
+++ 240915/조건에 따른 연산/operatino-by-rule.py
@@ -3,10 +3,9 @@ cnt = 0
 
 while n < 1000:
     if n % 2 == 0:
-        n = (n * 3) + 1
-        
-    if n % 2 == 1:
-        n = (n * 2) + 2
+        n = n * 3 + 1
+    else:
+        n = n * 2 + 2
     
     cnt += 1
 
